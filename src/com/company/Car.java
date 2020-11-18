790d0c6f6658c0a2637747a364ffdeafbe28b37f
moazzamsyntax <moazzamsyntax@gmail.com>
2020-11-18 17:58:09 -0500

    1st commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Car {
+
+    String type;
+    String color;
+    String model;
+    String Number;
+    int doors;
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Car audi =new Car();
+        audi.color="pink";
+        audi.Number="wefew";
+        audi.type="afsasf";
+        audi.doors=5;
+
+
+    }
+}
